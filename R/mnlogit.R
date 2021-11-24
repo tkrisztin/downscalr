7c8419ce417a58767f670f40781fce7314b9804a
tkrisztin <tkrisztin@gmail.com>
2021-11-24 23:46:11 +0100

    Styled and cleaned code of mnlogit

--- R/mnlogit.R
+++ R/mnlogit.R
@@ -3,15 +3,15 @@
 #' @param X An n by k matrix of explanatory variables
 #' @param Y An n by p matrix of dependent variables
 #' @param baseline Baseline class for estimation. Parameters will be set to zero. Defaults to the p-th column.
-#' @param ntot Total number of MCMC draws
+#' @param niter Total number of MCMC draws
 #' @param nburn Burn-in draws for MCMC
 #' @param A0 Prior for slope coefficients
 #'
 #'@details Performs MCMC estimation of a multinomial logit model as in Polson et al. (2012)
 #'
 #' @return A list containing
-#' * \code{postb} A k x p x (ntot - nburn dimensions) array containing posterior draws of the slope coefficients.
-#' * \code{marginal_fx} A k x p x (ntot - nburn dimensions) array containing posterior draws of marginal effects.
+#' * \code{postb} A k x p x (niter - nburn dimensions) array containing posterior draws of the slope coefficients.
+#' * \code{marginal_fx} A k x p x (niter - nburn dimensions) array containing posterior draws of marginal effects.
 #' * \code{X, Y, baseline} The matrices of explanatory and dependent variables, as defined above and the baseline class.
 #'
 #' @examples
@@ -26,32 +26,25 @@
 #'print(BETA)
 #'print(apply(res1$postb,c(1,2),mean))
 mnlogit <- function(X, Y, baseline = ncol(Y),
-                    ntot = 1000, nburn = 500, A0 = 10^4) {
-  ### let us assign prior values
-  # beta mean and variance are proper, but with high variance (= low prior information)
+                    niter = 1000, nburn = 500, A0 = 10^4) {
   n <- nrow(X)
   k <- ncol(X)
   p <- ncol(Y)
   pp <- (1:p)[-baseline]
+  # prior setup
   nn <- matrix(1, nrow(X), ncol(Y))
-
   beta_prior_mean <- matrix(0, k, p)
   beta_prior_var <- diag(k) * A0
 
   ### set-up the gibbs sampler
-  # total number of draws
-  niter <- ntot
-  # retain only S2 and discard S1, with S = S1 + S2
   ndiscard <- nburn
   nretain <- niter - ndiscard
   # save the posterior draws here
   postb <- array(0, c(k, p, nretain))
-  posty <- array(0, c(n, p, nretain))
-  postom <- array(0, c(n, p, nretain))
+  dimnames(postb)[[1]] <- colnames(X)
+  dimnames(postb)[[2]] <- colnames(Y)
 
   # starting values (won't matter after sufficient draws)
-  # curr.beta = mvrnorm(1,beta_prior_mean,beta_prior_var)
-  # curr.beta = solve(crossprod(X)) %*% crossprod(X,Y)
   curr.beta <- matrix(0, ncol = p, nrow = k)
   curr.beta[, p] <- 0
   curr.xb <- X %*% curr.beta
@@ -68,54 +61,38 @@ mnlogit <- function(X, Y, baseline = ncol(Y),
       A <- rowSums(exp(X %*% curr.beta[, -j]))
       c.j <- log(A)
       eta.j <- X %*% curr.beta[, j] - c.j
-
       # sample omega
       curr.om[, j] <- BayesLogit::rpg(n, nn[, j], eta.j)
-
       # draw beta
       V <- solve(beta_prior_var_inv + t(X) %*% (X * curr.om[, j]))
       b <- V %*% (beta_prior_var_inv %*% beta_prior_mean[, j] + t(X) %*% (kappa[, j] + c.j * curr.om[, j]))
       curr.beta[, j] <- MASS::mvrnorm(1, b, V)
     }
-
     # we are past the burn-in, save the draws
     if (iter > ndiscard) {
       s <- iter - ndiscard
       postb[, , s] <- curr.beta
       curr.xb <- X %*% curr.beta
-      posty[, , s] <- exp(curr.xb) / rowSums(exp(curr.xb))
-      postom[, , s] <- curr.om
     }
     utils::setTxtProgressBar(pb,iter)
   }
   close(pb)
-  ### calculate posterior mean of beta and sigma
-  beta_mean_hat <- apply(postb, c(1, 2), mean)
-  y_mean_hat <- apply(posty, c(1, 2), mean)
-
+  ### marginal effects calculations
   marginal_fx <- array(0, c(k, p, nretain))
   dimnames(marginal_fx)[[1]] <- colnames(X)
   dimnames(marginal_fx)[[2]] <- colnames(Y)
 
   meanXs <- apply(X, c(2), mean)
-
-  jjj <- 1
   for (jjj in 1:nretain) {
-    ppp <- 1
-
     MU <- X %*% postb[, , jjj]
     pr <- exp(MU) / rowSums(exp(MU))
-
     for (ppp in 1:p) {
       bbb <- matrix(1, n, k) %*% diag(postb[, ppp, jjj])
       pr_bbb <- bbb
       for (kk in 1:k) {
         pr_bbb[, kk] <- rowSums(pr %*% diag(postb[kk, , jjj]))
       }
-
       partial1 <- pr[, ppp] * (bbb - pr_bbb)
-
-
       marginal_fx[, ppp, jjj] <- apply(partial1, c(2), mean)
     }
   }
