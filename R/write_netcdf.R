e29b27d06bc7d9a076784c4c1ee6898b508b6efb
Leopold Ringwald <leopoldringwald@Leopolds-MBP.iiasa.ac.at>
2023-02-14 17:50:30 +0100

    redo write_netcdf function

--- R/write_netcdf.R
+++ R/write_netcdf.R
@@ -1,210 +1,235 @@
-#' Function to write results from downscalr into NetCDF file format
+#' Function to write results from downscalr or other sources into netCDF file format
 #'
 #'
-#' @param res Result from downscale
+#' @param data If not provided in \code{variables}, either a dataframe or matrix object with columns ns, times (optional), lu.to and value; defaults to NULL
 #' @param rasterfile RasterLayer object with ns as values
-#' @param filename Name and path of NetCDF file
-#' @param raster.crs CRS of raster
-#'
+#' @param filename Name of the output netCDF. Defaults to "Downscale_netCDF.nc"
+#' @param variables A list containing:
+#' * \code{data} = a data frame object with columns ns, times (optional), lu.to and value. Overridden if argument \code{data} is provided, defaults to NULL
+#' * \code{name_long} = character string specifying a long descriptive name; defaults to name_long="This is long name"
+#' * \code{name} = character string specifying the standard name, contains no whitespace and is case sensitive : defaults to name="this_is_standard_name"
+#' * \code{units} = character string specifying the unit of measure of data; defaults to units="unit_of_measurement"
+#' * \code{dimn} = integer specifying the number of dimensions; defaults to NULL
+#' * \code{varn} = integer specifying the number of levels of variable; defaults to NULL
+#' * \code{timen} = integer specifiying the number of timesteps 
+#' * \code{datadim} = integer specifying the dimension of data; defaults to 1
+#' * \code{expandValue} = numeric value or NA to fill in missing observations in \code{data}
+#' * \code{vardescr} = character string providing longer description of variable levels in data; defaults to NULL
+#' * \code{create.dimvar} = either TRUE or FALSE, defines if dimension variable of \code{data} should be created or not; defaults to TRUE
+#' @param start.time Integer defining first time step of time dimension to write, if dimension \code{times} is provided in \code{data} or \code{variables}, ; defaults to min(data$times) per variable
+#' @param end.time Integer defining last time step of time dimension to write, if dimension \code{times} is provided in \code{data} or \code{variables}, ; defaults to min(data$times) per variable
+#' @param by.time Integer specifying interval of time steps to be written between \code{start.time} and \code{end.time}; defaults to 10 
+#' @param filename Character string of netCDF file name, has to include ".nc" extension; defaults to filename="Downscale_netCDF.nc"
+#' @param filepath Character string to specify file path netCDF is written to, defaults to current working directory
+#' 
 #' @return
-#' * \file{write_netcdf} with name and path specified in filename function argument
-
+#' * Outputs \file{netCDF} file with \code{filename} written to \code{path} as specified in function argument
+#'
 #'
 #' @export write_netcdf
 #'
-#' @importFrom raster extent area setValues getValues res as.matrix
-#' @import ncdf4
-#' @import sp
-#'
-#' @examples
-#' require(dplyr)
-#' require(tidyr)
-#' require(tibble)
-#' require(ncdf4)
-#' require(raster)
 #' 
-#' ## A basic example to plot the observed LU changes
-#'
-#' areas <- data.frame(ns=getValues(argentina_raster),
-#'                     area=getValues(area(argentina_raster))) %>%
-#'          na.omit() %>%
-#'          group_by(ns) %>%
-#'          summarise(area=sum(area)) %>%
-#'          mutate(ns=as.character(ns))
-#'
-#' to.plot <- argentina_luc %>%
-#'            rename("times"="Ts") %>%
-#'            left_join(areas) %>%
-#'            mutate(value=value*area) %>%
-#'            dplyr::select(-area)
-#'
-#' \dontrun{write_netcdf(to.plot ,argentina_raster, filename="argentina.nc")}
-#'
-write_netcdf <- function(res,
-                         rasterfile,
-                         filename = "nc_v1.nc",
-                         raster.crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0") {
-  # define NULL values because package error checking does not deal with dplyr correctly
-  times = ns = lu.to = value = label = . = NULL
+#' @import ncdf4
+#' @importFrom terra crs values ext res setValues
+#' @import sp
+#' @import dplyr
+#' 
+#' 
+#' @examples
+#' 
+# start.year=2020
+# label <- "label"
+# units <- "km2"
+# filename <- "./testMW.nc"
+# 
+write_netcdf <- function(data=NULL, rasterfile=NULL, variables=list(name_long="This is long name", name="this_is_standard_name", units="unit_of_measurement", dimname="dim_name", dimn=3, varn=NULL, timen=NULL, datadim=1, expandValue=0, data=NULL, vardescr=NULL, create.dimvar=TRUE), start.time=NULL, end.time=NULL, by.time=10, filename = NULL, filepath=NULL){
   
-  p4s = sp::CRS(raster.crs)
-  na_sum = function(x) {
-    return(sum(x[!is.na(x)]))
+  
+  
+  #input check
+  if(is.null(start.time) & !is.null(data)) start.time <- min(data$times)
+  if(is.null(end.time) & !is.null(data)) end.time <- max(data$times)
+  if(is.null(by.time)) by.time <- 10
+  if(is.null(filename)) filename <- "Downscale_netCDF.nc"
+  if(is.null(filepath)) path <- paste0(getwd())
+  
+  
+  if(is.matrix(data) | is.data.frame(data) | !is.null(data)){
+    path <- paste0(getwd())
+    if (is.matrix(data)) data <- as.data.frame(data)
+    res <- data %>% dplyr::select(times,ns,lu.to,value) %>% rename(var1="lu.to", times="times") %>% group_by(ns,var1,times) %>% summarize(value=sum(value)) %>% ungroup()
+    
+    data_layer <- variables
+    data_layer$data <- as.data.frame(res)
+    data_layer$varn <- length(unique(res$var1))
+    
+    if(any(colnames(res) %in% "times")) data_layer$timen <- length(unique(res$times))                   
+    
+    variables <- list(data_layer)
+    
   }
-  geosims <- rasterfile
-  sp::proj4string(geosims) <- p4s
-  save_geovals <- data.frame(raster::getValues(geosims))
-  colnames(save_geovals) <- "ns"
   
-  geosims_area <- raster::area(geosims)
-  ttemp = raster::getValues(geosims_area)
-  ttemp[is.na(raster::getValues(geosims))] = NA
-  geosims_area = raster::setValues(geosims_area, ttemp)
+  on.exit(nc_close(ncid_out))
   
   
+  p4s = as.character(terra::crs(rasterfile))
+  na_sum = function(x) {return(sum(x[!is.na(x)]))}
+  geosims <- terra::rast(rasterfile)
+  #sp::proj4string(geosims) <- p4s
+  geosims <- terra::project(geosims,p4s)
+  save_geovals <- data.frame(terra::values(geosims)) 
+  colnames(save_geovals)<-"ns"
+  geosims_extent <- terra::ext(geosims)
   
   # Define some straightforward dimensions
-  lon <- seq(
-    raster::extent(geosims)[1] +
-      (raster::res(geosims)[2] / 2),
-    raster::extent(geosims)[2] -
-      (raster::res(geosims)[2] / 2),
-    length.out = dim(geosims)[2]
-  )
-  lat <- seq(
-    raster::extent(geosims)[4] - (raster::res(geosims)[1] / 2),
-    raster::extent(geosims)[3] + (raster::res(geosims)[1] / 2),
-    length.out = dim(geosims)[1]
-  )
-  time <- sort(unique(res$times))
-  lc_class <- sort(unique(res$lu.from))
+  lon <- seq(geosims_extent[1]+(terra::res(geosims)[2]/2),geosims_extent[2]-(terra::res(geosims)[2]/2),length.out=dim(geosims)[2])
+  lat <- seq(geosims_extent[4]-(terra::res(geosims)[1]/2),geosims_extent[3]+(terra::res(geosims)[1]/2),length.out=dim(geosims)[1])
+  time <- sort(seq.int(start.time,end.time,by.time))
+  
+  dim_lon <- ncdf4::ncdim_def("longitude", "degrees_east",lon,longname=NULL)
+  dim_lat <- ncdf4::ncdim_def("latitude", "degrees_north",lat,longname=NULL)
+  dim_time <- ncdf4::ncdim_def("time", "years", calendar="standard",time,unlim=TRUE,longname=NULL)
   
   
-  dim_lon <- ncdf4::ncdim_def("lon", "degrees_east", lon)
-  dim_lat <- ncdf4::ncdim_def("lat", "degrees_north", lat)
-  dim_time <- ncdf4::ncdim_def("time", "years", time, unlim = TRUE)
-  dim_lc_class <- ncdf4::ncdim_def("lc_class",
-                                   paste(paste0(seq(
-                                     1, length(lc_class), 1
-                                   ), "=", lc_class), collapse = "/"), seq(1, length(lc_class), 1))
-  dim_area <- ncdf4::ncdim_def("area", paste0("land_area"), 1)
-  
-  # create variables
   fillvalue <- NA
-  LandCover_pixshare_long <-
-    'share of pixel occupied by various land covers'
-  LandCover_pixshare <-
-    ncdf4::ncvar_def(
-      name = "LC_area_share",
-      units = "share of pixel area",
-      longname = LandCover_pixshare_long,
-      dim = list(dim_lon, dim_lat, dim_lc_class, dim_time),
-      missval = fillvalue,
-      prec = "double",
-      compression = 9
-    )
-  
-  PixelArea_long <- paste0('Total land area cover')
-  PixelArea <-
-    ncdf4::ncvar_def(
-      name = paste0('Land_area'),
-      units = "as input (km2)",
-      longname = PixelArea_long,
-      dim = list(dim_lon, dim_lat, dim_area),
-      missval = fillvalue,
-      prec = "double",
-      compression = 9
-    )
-  
-  
-  
-  ncid_out <-
-    ncdf4::nc_create(filename, list(LandCover_pixshare, PixelArea))
-  
-  
-  ### for the LU casef
-  #add the declaration
-  # reorder classes to match template
-  res_file_LU <- res %>%
-    group_by(times, ns, lu.to) %>%
-    summarise(value = sum(value)) %>%
-    rename(label = lu.to) %>%
-    pivot_wider(
-      id_cols = c(times, ns),
-      names_from = "label",
-      values_from = "value"
-    ) %>%
-    ungroup() %>%
-    dplyr::mutate(across(!c(ns, times), ~ ifelse(is.na(.), 0, .))) %>%
-    mutate(ns_area = rowSums(dplyr::select(., !c(ns, times)))) %>%
-    mutate(across(!c(ns, times, ns_area), ~ . / ns_area))
-  
-  ns_area <- res_file_LU %>%
-    subset(times == min(times)) %>%
-    dplyr::select(c(ns, ns_area))
-  
-  res_file_LU <- res_file_LU %>%
-    dplyr::select(-ns_area) %>%
-    filter(times >= min(times)) %>%
-    pivot_longer(
-      cols = !c(ns, times) ,
-      names_to = "label",
-      values_to = "value"
-    ) %>%
-    group_by(times, ns, label) %>%
-    arrange(label) %>%
-    pivot_wider(
-      id_cols = ns,
-      names_from = c(times, label),
-      values_from = value
-    ) %>%
-    left_join(ns_area) %>%
-    mutate(ns = as.numeric(ns)) %>%
-    ungroup()
-  
-  ns_area <- data.frame(ns_area)
-  
-  
-  
-  
-  o = match(save_geovals$ns, res_file_LU$ns)
-  to_plot2 = data.frame(res_file_LU[o, ])
-  
-  
-  
-  step = 2
-  for (t in 1:length(time)) {
-    for (c in 1:length(lc_class)) {
-      temp_map <- raster::setValues(geosims, as.numeric(to_plot2[, step]))
+  nc_var_list <- list()
+  nc_plot_list <- list()
+  label_name_list <- list()
+  label_num_list <- list()
+  for(ll in seq_len(length(variables))){
+    
+    
+    layer_temp <- variables[[ll]]
+    dim_temp_list <- list(dim_lon,dim_lat)
+    label_names_temp_list <- list()
+    
+    #if(layer_temp$dimn>2){  
+    for(kk in seq_len(layer_temp$datadim)){ 
+      label_names_temp <- gsub("[.]"," ",unique(layer_temp$data[,paste0('var',kk)]))
+      label_num_temp <- seq(1,length(label_names_temp),1)
       
-      data <- raster::as.matrix(temp_map)
-      ncdf4::ncvar_put(
-        ncid_out,
-        LandCover_pixshare,
-        t(data),
-        start = c(1, 1, c, t),
-        count = c(ncol(data), nrow(data), 1, 1)
-      )
-      step = step + 1
+      if(layer_temp$create.dimvar) dim_temp <- ncdf4::ncdim_def(paste0(layer_temp$dimname[kk],"_legend"),paste(paste(paste0(seq(1,length(label_names_temp),1)," = ",label_names_temp), collapse="; ")), label_num_temp, longname=NULL,create_dimvar=layer_temp$create.dimvar) else dim_temp <- ncdf4::ncdim_def(paste0(layer_temp$dimname[kk],"_legend"),"", as.integer(label_num_temp), longname=NULL,create_dimvar=FALSE)
+      
+      dim_temp_list[[kk+2]] <- dim_temp 
+      label_names_temp_list[[kk]] <- label_names_temp
     }
+    # }else{
+    #   kk <- 0
+    #   }
+    
+    
+    if(!is.na(layer_temp$timen)){
+      
+      if(is.null(start.time)) start.time <- min(layer_temp$timen)
+      if(is.null(end.time)) end.time <- max(layer_temp$timen)
+      
+      layer_temp_df <- layer_temp$data %>% filter(times>=start.time, times<=end.time) %>% ungroup() %>% group_by(times,across(all_of(grep('var',colnames(layer_temp$data),value=TRUE)))) %>% arrange(times,across(all_of(grep('var',colnames(layer_temp$data),value=TRUE))), .by_group=TRUE) %>%
+        pivot_wider(id_cols = ns, names_from = c(times,grep('var',colnames(layer_temp$data),value=TRUE)), values_from = value)
+      
+      layer_temp_df[is.na(layer_temp_df)] <- layer_temp$expandValue
+      
+      layer_temp_df <- data.frame(layer_temp_df)
+      
+      
+      dim_temp_list[[kk+3]] <- dim_time
+      
+      temp_nc_var <- ncvar_def(name = layer_temp$name, 
+                               longname = layer_temp$name_long,
+                               dim = dim_temp_list, units = layer_temp$units,
+                               missval=fillvalue,prec = "double", compression = 9)
+      
+      
+    }else{
+      
+      layer_temp_df <- layer_temp$data %>% ungroup() %>% group_by(across(all_of(grep('var',colnames(layer_temp$data),value=TRUE)))) %>% arrange(across(all_of(grep('var',colnames(layer_temp$data),value=TRUE))), .by_group=TRUE) %>%
+        pivot_wider(id_cols = ns, names_from = c(grep('var',colnames(layer_temp$data),value=TRUE)), values_from = value)
+      
+      layer_temp_df[is.na(layer_temp_df)] <- layer_temp$expandValue
+      
+      layer_temp_df <- data.frame(layer_temp_df)
+      
+      
+      temp_nc_var <- ncvar_def(name = layer_temp$name,units = layer_temp$units,
+                               longname = layer_temp$name_long,
+                               dim = dim_temp_list, 
+                               missval=fillvalue,prec = "double",compression = 9)  
+      
+      
+      
+    }
+    # label and numbering
+    o_temp = match(save_geovals$ns,as.numeric(layer_temp_df[,c('ns')]))
+    to_plot_temp = data.frame(layer_temp_df[o_temp,])
+    colnames(to_plot_temp) <- gsub("[.]"," ",colnames(to_plot_temp))
+    
+    
+    
+    
+    #define layer specific dim
+    
+    
+    
+    
+    
+    nc_var_list[[ll]] <- temp_nc_var
+    nc_plot_list[[ll]] <- to_plot_temp
+    label_name_list[[ll]] <- label_names_temp_list
+    label_num_list[[ll]] <- label_num_temp
   }
+  ncid_out <- nc_create(paste0(path,"/",filename),nc_var_list,force_v4=TRUE)
+  ### add attributes
+  
+  for(pp in seq_len(length(nc_var_list))){
+    nc_var_temp <- nc_var_list[[pp]]
+    nc_plot_temp <- nc_plot_list[[pp]]
+    #ncatt_put(ncid_out, nc_var_temp$name,"levels","gicht",prec="text")
+    ncatt_put(ncid_out, nc_var_temp$name,"description",paste(paste(" ",variables[[pp]]$vardescr, collapse="; ")), prec="text")
+    
+    
+    for (ii in 1:(ncol(nc_plot_temp)-1)) {
+      c <- NULL;d <- NULL;t <- NULL
+      
+      
+      slct <-  colnames(nc_plot_temp)[ii+1]
+      if(!(is.na(variables[[pp]]$timen) | is.na(variables[[pp]]$timen))) t <- which(time==time[sapply(time,grepl,slct)]) else t <- NULL
+      print(slct)
+      c <- which(label_name_list[[pp]][[1]] %in% label_name_list[[pp]][[1]][sapply(label_name_list[[pp]][[1]],grepl,fixed=TRUE,slct)])
+      if(length(c)>1) c <- c[2]
+      if(length(variables[[pp]]$varn)>1) d <- which(label_name_list[[pp]][[2]]==label_name_list[[pp]][[2]][sapply(label_name_list[[pp]][[2]],grepl,fixed=TRUE,slct)])  else d <-  NULL
+      temp_map <- terra::setValues(geosims,nc_plot_list[[pp]][,slct])
+      
+      temp_data <- as.matrix(temp_map, wide=TRUE)
+      #data <- as.matrix(temp_map)
+      print(temp_map)
+      start_temp <- c(1,1,c,d,t)
+      count_temp <- c(ncol(temp_data),nrow(temp_data), rep(1,length(start_temp)-2))
+      print(start_temp)
+      ncvar_put(ncid_out, nc_var_temp, t(temp_data), start=start_temp,
+                count=count_temp)
+      
+      #print(step)
+      
+      
+      #print(step)
+    } 
+    
+    
+    
+  } 
   
   
-  o2 = match(save_geovals$ns, res_file_LU$ns)
-  to_plot_area = data.frame(ns_area[o2, ])
-  temp_map <- raster::setValues(geosims, as.numeric(to_plot_area[, 2]))
-  
-  data <- raster::as.matrix(temp_map)
-  
-  ncdf4::ncvar_put(
-    ncid_out,
-    PixelArea,
-    t(data),
-    start = c(1, 1, 1),
-    count = c(ncol(data), nrow(data), 1)
-  )
+  #ncatt_put(ncid_out,0, "title", path)
+  ncatt_put(ncid_out,0, "institution", "IIASA")
+  history <- paste("Leopold J.C. Ringwald", date(), sep=", ")
+  ncatt_put(ncid_out,0, "history", history)
+  ncatt_put(ncid_out,0, "raster metadata",  paste("CRS: ",as.character(terra::crs(rasterfile)),"   Extent:", paste(geosims_extent[1:4],collapse=", "), " (xmin,xmax,ymin,ymax)", "   Resolution:", paste(terra::res(rasterfile), collapse=";")))  
   
   
+  #nc_close(ncid_out)
   
-  ncdf4::nc_close(ncid_out)
+  #label_return <- list(label_table=label_tbl, label_map=label_full_map)
+  #write.table(label_return$label_table, file=paste0("./outputs/LabelReadme_",gdx.path,'netCDF_LEO.txt'), quote=FALSE, sep="                                                       ", row.names=FALSE)
+  #write.csv(label_return$label_map, file=paste0("./outputs/LabelMap_",gdx.path,'netCDF_LEO.csv'), row.names=FALSE)
 }
+
+
